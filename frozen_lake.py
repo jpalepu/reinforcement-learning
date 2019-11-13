22798d903ce49f657d703b3bb3aa931a2b5e0053
jithin kumar <44967770+jithin-pal@users.noreply.github.com>
2019-11-13 07:45:41 +0530

    Add files via upload

--- frozen_lake.py
+++ frozen_lake.py
@@ -1,121 +1,77 @@
-import tensorflow as tf
-import numpy as np
+#!/usr/bin/env python3
 import gym
-import matplotlib.pyplot as plt
+import collections
+from tensorboardX import SummaryWriter
+
+ENV_NAME = "FrozenLake-v0"
+GAMMA = 0.9
+ALPHA = 0.2
+TEST_EPISODES = 20
 
 
-tf.set_random_seed(1)
-np.random.seed(1)
+class Agent:
+    def __init__(self):
+        self.env = gym.make(ENV_NAME)
+        self.state = self.env.reset()
+        self.values = collections.defaultdict(float)
 
-class DQN():
-    def __init__(self,nstate,naction):
-        self.nstate=nstate
-        self.naction=naction
-        self.sess = tf.Session()
-        self.memcnt=0
-        self.BATCH_SIZE = 64
-        self.LR = 0.0012                      # learning rate
-        self.EPSILON = 0.92                 # greedy policy
-        self.GAMMA = 0.9999                   # reward discount
-        self.MEM_CAP = 2000
-        self.mem= np.zeros((self.MEM_CAP, self.nstate * 2 + 2))     # initialize memory
-        self.updataT=150
-        self.built_net()
+    def sample_env(self):
+        action = self.env.action_space.sample()
+        old_state = self.state
+        new_state, reward, is_done, _ = self.env.step(action)
+        self.state = self.env.reset() if is_done else new_state
+        return (old_state, action, reward, new_state)
 
+    def best_value_and_action(self, state):
+        best_value, best_action = None, None
+        for action in range(self.env.action_space.n):
+            action_value = self.values[(state, action)]
+            if best_value is None or best_value < action_value:
+                best_value = action_value
+                best_action = action
+        return best_value, best_action
 
-    def built_net(self):
-        self.s = tf.placeholder(tf.float64, [None,self.nstate])
-        self.a = tf.placeholder(tf.int32, [None,])
-        self.r = tf.placeholder(tf.float64, [None,])
-        self.s_ = tf.placeholder(tf.float64, [None,self.nstate])
+    def value_update(self, s, a, r, next_s):
+        best_v, _ = self.best_value_and_action(next_s)
+        new_val = r + GAMMA * best_v
+        old_val = self.values[(s, a)]
+        self.values[(s, a)] = old_val * (1-ALPHA) + new_val * ALPHA
 
-        with tf.variable_scope('q'):                                  # evaluation network
-            l_eval = tf.layers.dense(self.s, 10, tf.nn.relu, kernel_initializer=tf.random_normal_initializer(0, 0.1))
-            self.q = tf.layers.dense(l_eval, self.naction, kernel_initializer=tf.random_normal_initializer(0, 0.1))
-
-        with tf.variable_scope('q_next'):                                           # target network, not to train
-            l_target = tf.layers.dense(self.s_, 10, tf.nn.relu, trainable=False)
-            q_next = tf.layers.dense(l_target, self.naction, trainable=False)
-
-        q_target = self.r + self.GAMMA * tf.reduce_max(q_next, axis=1)    #q_next:  shape=(None, naction),
-        a_index=tf.stack([tf.range(self.BATCH_SIZE,dtype=tf.int32),self.a],axis=1)
-        q_eval=tf.gather_nd(params=self.q,indices=a_index)
-        loss=tf.losses.mean_squared_error(q_target,q_eval)
-        self.train=tf.train.AdamOptimizer(self.LR).minimize(loss)
-        #  q现实target_net- Q估计
-        self.sess.run(tf.global_variables_initializer())
-
-    def choose_action(self,status):
-        fs = np.zeros((1,self.nstate))
-        fs[0,status]=1.0  # ONE HOT
-        if  np.random.uniform(0.0,1.0)<self.EPSILON:
-            action=np.argmax( self.sess.run(self.q,feed_dict={self.s:fs}))
-        else:
-            action=np.random.randint(0,self.naction)
-        return action
-
-    def learn(self):
-        if(self.memcnt%self.updataT==0):
-            t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='q_next')
-            e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='q')
-            self.sess.run([tf.assign(t, e) for t, e in zip(t_params, e_params)])
-        rand_indexs=np.random.choice(self.MEM_CAP,self.BATCH_SIZE,replace=False)
-        temp=self.mem[rand_indexs]
-        bs = temp[:,0:self.nstate]#.reshape(self.BATCH_SIZE,NSTATUS)
-        ba = temp[:,self.nstate]
-        br = temp[:,self.nstate+1]
-        bs_ = temp[:,self.nstate+2:]#.reshape(self.BATCH_SIZE,NSTATUS)
-        self.sess.run(self.train, feed_dict={self.s:bs,self.a:ba,self.r:br,self.s_:bs_})
+    def play_episode(self, env):
+        total_reward = 0.0
+        state = env.reset()
+        while True:
+            _, action = self.best_value_and_action(state)
+            env.render()
+            new_state, reward, is_done, _ = env.step(action)
+            total_reward += reward
+            if is_done:
+                break
+            state = new_state
+        return total_reward
 
 
-    def storeExp(self,s,a,r,s_):
-        fs = np.zeros(self.nstate)
-        fs[s] = 1.0                       # ONE HOT
-        fs_ = np.zeros(self.nstate)
-        fs_[s_] = 1.0                          # ONE HOT
-        self.mem[self.memcnt%self.MEM_CAP]=np.hstack([fs,a,r,fs_])
-        self.memcnt+=1
+if __name__ == "__main__":
+    test_env = gym.make(ENV_NAME)
+    agent = Agent()
+    writer = SummaryWriter(comment="-q-learning")
 
+    iter_no = 0
+    best_reward = 0.0
+    while True:
+        iter_no += 1
+        s, a, r, next_s = agent.sample_env()
+        agent.value_update(s, a, r, next_s)
 
-    def run(self,numsteps):
-        cnt_win =0
-        all_r=0.0
-        win_rate=[]
-        for i in range(numsteps):
-            s=env.reset()
-            done=False
-            while(not done):
-                a=self.choose_action(s)
-                s_,r,done,_=env.step(a)
-                all_r+=r
-                self.storeExp(s,a,r,s_)
-                if(self.memcnt>self.MEM_CAP):
-                    self.learn()
-                    if(done):
-                        if(s_==self.nstate-1):
-                            cnt_win+=1.0
-                s=s_
-            if (i % 50 == 0):
-                print("period: ",i, ": ")
-                if (cnt_win / 50 > 0.4):
-                     self.EPSILON += 0.01;
-                elif (cnt_win / 50 > 0.2):
-                      self.EPSILON += 0.005;
-                elif (cnt_win / 50 > 0.1):
-                      self.EPSILON += 0.003;
-                elif (cnt_win / 50 > 0.05):
-                      self.EPSILON += 0.001;
-
-                print("current accuracy: %.2f %%" %(cnt_win/50.0*100))
-                win_rate.append(cnt_win / 50)
-                cnt_win=0
-                print("Global accuracy : %.2f %%" %(all_r / (i+1)*100))
-        print("Global accuracy : ",all_r/numsteps*100,"%")
-        plt.plot(win_rate)
-        plt.show()
-
-env = gym.make('FrozenLake-v0')
-#env = gym.make('FrozenLake8x8-v0')
-env = env.unwrapped
-dqn=DQN(env.observation_space.n,env.action_space.n)
-dqn.run(2000)
+        reward = 0.0
+        for _ in range(TEST_EPISODES):
+            reward += agent.play_episode(test_env)
+        reward /= TEST_EPISODES
+        writer.add_scalar("reward", reward, iter_no)
+        if reward > best_reward:
+            print("Best reward updated %.3f -> %.3f" % (best_reward, reward))
+            best_reward = reward
+        if reward > 0.80:
+            print("Solved in %d iterations!" % iter_no)
+            break
+    writer.close()
